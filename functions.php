3f3fc379b7d6d44583528a33e7933930d7d61fee
Leland Fiegel <lelandf@gmail.com>
2016-01-13 12:20:44 -0500

    Title tag support, wp_head/footer hooks, enqueue stylesheet, output menu location, add screen-reader-text CSS

--- functions.php
+++ functions.php
@@ -17,6 +17,14 @@ if ( ! function_exists( 'social_menu_theme_setup' ) ) :
  */
 function social_menu_theme_setup() {
 
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'social-icon-font' => esc_html__( 'Social Icon Font Menu', 'social-menu-theme' ),
@@ -25,3 +33,11 @@ function social_menu_theme_setup() {
 }
 endif;
 add_action( 'after_setup_theme', 'social_menu_theme_setup' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function social_menu_theme_scripts() {
+	wp_enqueue_style( 'social-menu-theme-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'social_menu_theme_scripts' );
--- index.php
+++ index.php
@@ -12,3 +12,24 @@
  * @package Social_Menu_Theme
  */
 ?>
+<!doctype html>
+<html>
+	<head>
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<?php 
+			if ( has_nav_menu( 'social-icon-font' ) ) :
+				wp_nav_menu( array(
+					'theme_location'  => 'social-icon-font',
+					'depth'           => 1,
+					'link_before'     => '<span class="screen-reader-text">',
+					'link_after'      => '</span>',
+					'container_class' => 'social-icon-font',
+				) );
+			endif;
+		?>
+
+	<?php wp_footer(); ?>
+	</body>
+</html>
